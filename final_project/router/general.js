ad838c4922bdcfcbbb7e8b9ca80cde67c50ebc1f
juskyna22 <66147907+juskyna22@users.noreply.github.com>
2023-12-01 19:44:19 -0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -15,12 +15,12 @@ public_users.post("/register", (req,res) => {
   if (username && password) {
     if (!isValid(username)) {
       users.push({"username":username, "password":password});
-      return res.status(200).json({message: "User successfully registred. Now you can login"});
+      return res.status(200).json({message: "You are now part of the team. Log in and let the fun begin"});
     } else {
-      return res.status(404).json({message: "User already exists!"});
+      return res.status(404).json({message: "You are already a premium member!"});
     }
   }
-  return res.status(404).json({message: "Unable to register user."});
+  return res.status(404).json({message: "Registration unavailable."});
 });
 
 /* express.get() is a method meant to serve the retrieve requests to the server. It has two parameters;first defines the end point and 
