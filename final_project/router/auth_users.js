c77074db96cac49c4c98ee9beb0284c0735ef3ef
juskyna22 <66147907+juskyna22@users.noreply.github.com>
2023-12-01 20:32:33 -0700

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -8,10 +8,10 @@ let users = [];
 const isValid = (username)=>{ //returns boolean
     //write code to check is the username is valid
 
-    let userswithsamename = users.filter((user)=>{
+    let sameName = users.filter((user)=>{
         return user.username === username
     });
-    if(userswithsamename.length > 0){
+    if(sameName.length > 0){
         return true;
     } else {
         return false;
