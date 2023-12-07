ee71eee7916fa5069996f1cf44ccda3199e05fbf
juskyna22 <66147907+juskyna22@users.noreply.github.com>
2023-12-07 14:44:09 -0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -61,31 +61,23 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-    const get_books_author = new Promise((resolve, reject) => {
+  byAuthor = req.params.author;
+  booksCollection = books;
+  newCollection = {};
+  
+  for(var key in booksCollection) {
+      if(booksCollection.hasOwnProperty(key)) {
+          var value = booksCollection[key];
+          if  (value["author"] == byAuthor) {
+              newCollection[key] = value;
+          }
 
-    let booksbyauthor = [];
-    let isbns = Object.keys(books);
-    isbns.forEach((isbn) => {
-      if(books[isbn]["author"] === req.params.author) {
-        booksbyauthor.push({"isbn":isbn,
-                            "title":books[isbn]["title"],
-                            "reviews":books[isbn]["reviews"]});
-      resolve(res.send(JSON.stringify({booksbyauthor}, null, 4)));
       }
-
-
-    });
-    reject(res.send("The mentioned author does not exist "))
-        
-    });
-
-    get_books_author.then(function(){
-            console.log("Promise is resolved");
-   }).catch(function () { 
-                console.log('The mentioned author does not exist');
+  }
+  res.send(newCollection);
+      
   });
-
-  });
+  
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
