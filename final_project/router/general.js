db7151507ddfbb3efff1c11af7e7158b9bd18815
juskyna22 <66147907+juskyna22@users.noreply.github.com>
2023-12-07 14:32:08 -0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,10 +28,13 @@ second is a function taking the request-handler and the response handler*/
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  res.send(JSON.stringify(books, null, 4));
-  
-});
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(JSON.stringify({books}, null, 4)));
+      });
+
+      get_books.then(() => console.log("Promise for Task 10 resolved"));
+
+  });
 
 
 // Get book details based on ISBN
@@ -58,23 +61,31 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  byAuthor = req.params.author;
-  booksCollection = books;
-  newCollection = {};
-  
-  for(var key in booksCollection) {
-      if(booksCollection.hasOwnProperty(key)) {
-          var value = booksCollection[key];
-          if  (value["author"] == byAuthor) {
-              newCollection[key] = value;
-          }
+    const get_books_author = new Promise((resolve, reject) => {
 
+    let booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["author"] === req.params.author) {
+        booksbyauthor.push({"isbn":isbn,
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});
+      resolve(res.send(JSON.stringify({booksbyauthor}, null, 4)));
       }
-  }
-  res.send(newCollection);
-      
+
+
+    });
+    reject(res.send("The mentioned author does not exist "))
+        
+    });
+
+    get_books_author.then(function(){
+            console.log("Promise is resolved");
+   }).catch(function () { 
+                console.log('The mentioned author does not exist');
   });
-  
+
+  });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
